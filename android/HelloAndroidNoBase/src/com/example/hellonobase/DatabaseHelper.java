ad533a2e8b2da9d063d28946d6a5d79f1dbcead5
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-10-05 14:42:54 +0000

    Fixed database name.

--- android/HelloAndroidNoBase/src/com/example/hellonobase/DatabaseHelper.java
+++ android/HelloAndroidNoBase/src/com/example/hellonobase/DatabaseHelper.java
@@ -18,7 +18,7 @@ import com.j256.ormlite.table.TableUtils;
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
 	// name of the database file for your application -- change to something appropriate for your app
-	private static final String DATABASE_NAME = "helloAndroid.db";
+	private static final String DATABASE_NAME = "helloNoBase.db";
 	// any time you make changes to your database objects, you may have to increase the database version
 	private static final int DATABASE_VERSION = 1;
 
