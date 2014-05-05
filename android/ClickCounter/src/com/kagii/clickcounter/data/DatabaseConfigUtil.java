56c202136b3219fa359428b1af67911db5f8c522
Nima <nimaghafoori@gmx.com>
2014-05-05 17:43:24 +0200

    Update DatabaseConfigUtil.java

--- android/ClickCounter/src/com/kagii/clickcounter/data/DatabaseConfigUtil.java
+++ android/ClickCounter/src/com/kagii/clickcounter/data/DatabaseConfigUtil.java
@@ -6,8 +6,9 @@ import java.sql.SQLException;
 import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 
 /**
- * Database helper class used to manage the creation and upgrading of your database. This class also usually provides
- * the DAOs used by the other classes.
+ * DatabaseConfigUtl writes a configuration file to avoid using Annotation processing in runtime. This gains a
+ * noticable performance improvement. configuration file is written to /res/raw/ by default.
+ * More info at: http://ormlite.com/javadoc/ormlite-core/doc-files/ormlite_4.html#Config-Optimization
  */
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
 
